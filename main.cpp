44bd07530c629f9b0b5f04925fb7b6ad19ca4bbd
fojd <ericbmousy@gmail.com>
2025-01-26 23:51:45 +0100

    -add cars.bpm to project -get surface from window -add image to surface

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <SDL.h>
-#include <glad/glad.h>
 #include <iostream>
 
 #define W_WIDTH 1280
@@ -13,16 +12,6 @@ int main(int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL video system initialization: FAIL\n" << SDL_GetError() << "\n";
 	}
-	else {
-		std::cout << "SDL video system initialization: OK\n";
-	}
-
-	// before creating window, specify OpenGL version (4.1) and setup context
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
 	window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, W_WIDTH, W_HEIGHT, 
 		SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
@@ -30,17 +19,19 @@ int main(int argc, char* argv[]) {
 		std::cout << "SDL window creation: FAIL\n";
 		return 1;
 	}
+	
+	surface = SDL_GetWindowSurface(window);
+	if (!surface) {
+		std::cout << "SDL window surface fetch: FAIL\n";
+	}
 
-	// setup OpenGL context
-	SDL_GLContext context;
-	context = SDL_GL_CreateContext(window);
-
-	// setup function pointers
-	gladLoadGLLoader(SDL_GL_GetProcAddress);
+	SDL_Surface* image = SDL_LoadBMP("./cars.bmp");
+	SDL_BlitSurface(image, nullptr, surface, nullptr);
+	SDL_FreeSurface(image);
+	SDL_UpdateWindowSurface(window);
 
 	bool is_running = true;
 	while (is_running) {
-		glViewport(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, W_WIDTH, W_HEIGHT);
 
 		SDL_Event event;
 		// Queue of events to handle
@@ -57,10 +48,6 @@ int main(int argc, char* argv[]) {
 			const Uint8* kb_state = SDL_GetKeyboardState(nullptr);
 			if (kb_state[SDL_SCANCODE_F] == 1) std::cout << "f key down\n";
 		}
-
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(window);
 
 	}
 
