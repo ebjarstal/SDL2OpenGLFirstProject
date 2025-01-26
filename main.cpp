be505eac0382b493dd1e03ad43af6d905f56f516
fojd <ericbmousy@gmail.com>
2025-01-26 17:02:50 +0100

    quit and keydown events

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ int main(int argc, char* argv[]) {
 	SDL_Surface* surface = nullptr;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "SDL video system initialization: FAIL" << SDL_GetError() << std::endl;
+		std::cout << "SDL video system initialization: FAIL" << SDL_GetError() << "\n";
 	}
 	else {
-		std::cout << "SDL video system initialization: OK" << std::endl;
+		std::cout << "SDL video system initialization: OK\n";
 	}
 
 	window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, W_WIDTH, W_HEIGHT, SDL_WINDOW_SHOWN);
 	if (!window) {
-		std::cout << "SDL window creation: FAIL" << std::endl;
+		std::cout << "SDL window creation: FAIL\n";
 		return 1;
 	}
 
@@ -28,6 +28,12 @@ int main(int argc, char* argv[]) {
 		// Queue of events to handle
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) is_running = false;
+			if (event.type == SDL_KEYDOWN) {
+				// scancode: physical key
+				// sym: virtual key
+				// Usage depends on purpose of program: is the keyboard layout important?
+				if (event.key.keysym.sym == SDLK_LEFT) std::cout << "left arrow down\n";
+			}
 		}
 	}
 
